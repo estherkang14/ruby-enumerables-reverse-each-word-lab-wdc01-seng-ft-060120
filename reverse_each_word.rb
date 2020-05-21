affae2582c19ca33f15360a9a3dc03d28585d9f7
Esther Kang <estherkang14@gmail.com>
2020-05-21 03:30:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence) 
-  sentence.split(" ")
-  sentence.each do |string_word|
+  new_sentence = sentence.split(" ")
+  new_sentence.each do |string_word|
+    
     
 end 

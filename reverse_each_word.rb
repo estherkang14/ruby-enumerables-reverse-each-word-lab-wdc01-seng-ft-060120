21347b75bceb0d6a25d8224f7968701c5ec9b9e0
Esther Kang <estherkang14@gmail.com>
2020-05-21 03:40:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence) 
   new_sentence = sentence.split(" ")
  
-  new_sentence.each do |string_word|
-    string_word.reverse() 
-  end 
+  new_sentence.each { |string_word|
+    string_word
+  }
     
 end 
